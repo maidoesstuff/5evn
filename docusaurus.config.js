0d9556023d4192d6b20160e4bcfb312eb1915a75
maidoesstuff <103353870+maidoesstuff@users.noreply.github.com>
2022-12-12 17:59:22 +1100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,11 +96,7 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-               // label: 'Blog',
-               // to: '/blog',
-              },
-              {
+    {
                 label: 'GitHub',
                 href: 'https://github.com/maidoesstuff/5evn',
               },
